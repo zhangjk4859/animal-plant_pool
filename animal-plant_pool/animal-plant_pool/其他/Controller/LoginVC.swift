4761aee998eccca083fe9bca7186c4f44dbff1ab
zhangjk4859 <573581325@qq.com>
2017-04-07 10:32:13 +0800

    no message

--- animal-plant_pool/animal-plant_pool/其他/Controller/LoginVC.swift
+++ animal-plant_pool/animal-plant_pool/其他/Controller/LoginVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginVC: UIViewController,UITableViewDataSource,UITableViewDelegate{
+class LoginVC: UIViewController{
     var backgroundImageView:UIImageView?
     var tableView : UITableView?
 
@@ -19,69 +19,30 @@ class LoginVC: UIViewController,UITableViewDataSource,UITableViewDelegate{
         
         backgroundImageView = UIImageView(image: UIImage(named: "background"))
         view.backgroundColor = UIColor.white
-        //让图片滚动起来
-        let tableView = UITableView(frame:CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT * 0.5), style: UITableViewStyle.plain)
-        tableView.dataSource = self
-        tableView.delegate = self
-        view.addSubview(tableView)
-        self.tableView = tableView
+        view.addSubview(backgroundImageView!)
+        //颜色渐变
         
-        
-        
-        //添加三个按钮
-        let width = view.width / 3
+        //添加三个按钮,宽度平分成六份
+        let width = view.width / 6
         let height = width + 30
         var names = ["手机登录","微信登录","QQ登录"]
+        var imageNames = ["login_mobile","login_weixin","login_QQ"]
         for index in 0 ..< 3 {
             let btn = JKVerticalButton.init(type: UIButtonType.custom)
             view.addSubview(btn)
-            btn.frame = CGRect(x: CGFloat(Float(index)) * width, y: tableView.frame.maxY, width: width, height: height)
-            btn.backgroundColor = UIColor.blue
+            btn.frame = CGRect(x: CGFloat(Float(index)) * width * 2 + width * 0.5, y: backgroundImageView!.frame.maxY + width, width: width, height: height)
+        
             btn.setTitle(names[index], for: UIControlState.normal)
-            btn.setImage(UIImage(named:"release"), for: UIControlState.normal)
+            btn.titleLabel?.adjustsFontSizeToFitWidth = true
+            btn.setTitleColor(UIColor.black, for: .normal)
+            btn.setImage(UIImage(named:imageNames[index]), for: UIControlState.normal)
         }
         
         
 
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        autoScroll()
-    }
-    
-    //用UIView的动画进行滚动
-    func autoScroll(){
-        
-        UIView.animate(withDuration: 10) { 
-            self.tableView?.setContentOffset(CGPoint(x: 0, y: -2000), animated: false)
-        }
-        
-        
     }
 
 
 }
 
-extension LoginVC{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-         if cell == nil {
-           cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-            
-            let imageView = UIImageView(image: UIImage(named: "background"))
-            cell?.addSubview(imageView)
-//            cell?.textLabel?.text = "111"
-        }
-//        print(cell.)
-        return cell!
-        
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return (backgroundImageView?.height)!
-    }
-}
+
